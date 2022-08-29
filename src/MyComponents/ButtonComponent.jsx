65b0b8facbd44c3fc312500a8edcd02bcf8e1886
Massimo Manco <massimomanco80@gmail.com>
2022-08-29 16:55:28 +0200

    esercizi

---
+++ src/MyComponents/ButtonComponent.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react"
+
+const Button = () =>  {
+	return <button>Cliccami</button>
+}
+
+export default Button
---
+++ src/MyComponents/ImageComponent.jsx
@@ -0,0 +1,12 @@
+
+
+import { Component } from 'react'
+
+class ImageComponent extends Component {
+  render() {
+   
+    return <img src='../esperienza.jpeg'></img>
+  }
+}
+
+export default ImageComponent
